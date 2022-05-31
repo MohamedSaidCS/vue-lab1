7b54e97313ebf1a15fd37c5d8ecde141f6cfb109
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2022-05-31 22:24:42 +0200

    lab 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tabs">
+    <button @click="tab = 'form'">Form</button>
+    <button @click="tab = 'students'">Students</button>
+    <button @click="tab = 'admins'">Admins</button>
+  </div>
+  <FormComponent @add="addUser" v-if="tab === 'form'" />
+  <StudentsTable @delete="deleteUser" :students="students" v-if="tab === 'students'" />
+  <AdminsTable @delete="deleteUser" :admins="admins" v-if="tab === 'admins'" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FormComponent from "@/components/FormComponent";
+import StudentsTable from "@/components/StudentsTable";
+import AdminsTable from "@/components/AdminsTable";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AdminsTable,
+    StudentsTable,
+    FormComponent
+  },
+  data() {
+    return {
+      students: [],
+      admins: [],
+      tab: 'form',
+    }
+  },
+  methods: {
+    addUser(data) {
+      if (data.type === 'admin')
+        this.admins.push(data.user);
+      else
+        this.students.push(data.user);
+    },
+    deleteUser(data) {
+      if (data.type === 'admin')
+        this.admins.splice(data.index, 1);
+      else
+        this.students.splice(data.index, 1);
+    }
+  },
 }
+
 </script>
 
 <style>
+
+body {
+  margin: 0;
+  background-color: #7c80e4;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +61,30 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  padding: 1rem;
+  color: #7c80e4;
+  font-weight: bold;
+  background-color: black;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tabs button {
+  margin-inline: 0.5rem;
+}
+
+table, th, td {
+  margin: auto;
+  padding: 1px;
+  border: 1px solid black;
+}
+
 </style>
---
+++ src/components/AdminsTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Admins</h1>
+  <table>
+    <thead>
+    <tr>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Address</th>
+      <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(student,index) in admins" :key="index">
+      <td>{{ student.name }}</td>
+      <td>{{ student.age }}</td>
+      <td>{{ student.address }}</td>
+      <td>
+        <button @click="$emit('delete', {type: 'admin', index: index})">Delete</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "AdminsTable",
+  emits: ['delete'],
+  props: {
+    admins: Array
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
